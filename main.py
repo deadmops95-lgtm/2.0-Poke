657df97d3344ade33e45145b0e1fbe18e4c3f041
deadmops95-lgtm <deadmops95@gmail.com>
2026-09-22 02:51:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 import asyncio
 
 TOKEN = "8628464354:AAEQ0XKfv9OR-CR368dSaXq6tQsipn_Wy7w"
-DOMAIN = "bot-1790032438-3286-prokudin95.bothost.tech"
+# Твой новый актуальный домен с Bothost
+DOMAIN = "bot-1790034365-8732-prokudin95.bothost.tech"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
@@ -201,5 +202,4 @@ async def catch_all(full_path: str):
     return RedirectResponse(url="/", status_code=303)
 
 if __name__ == "__main__":
-    # Запускаем на порту 3000, который ждет Bothost
     uvicorn.run("main:app", host="0.0.0.0", port=3000)
